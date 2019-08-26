163bdc2b2ff6e1ca20cd5d96fb7921efe3c3c02d
Michael Alexander <michael_alexander_20@outlook.com>
2019-08-26 15:01:12 +0100

    Game/index.erb printed

--- video_game_shop/controllers/game_controller.rb
+++ video_game_shop/controllers/game_controller.rb
@@ -5,10 +5,10 @@ also_reload('../models/*')
 
 get '/games' do
   @games = Game.all()
-  erb (:"games/index")
+  erb (:"game/index")
 end
 
 get '/games/:id' do
   @games = Game.find(params['id'].to_i)
-  erb(:"games/show")
+  erb(:"game/show")
 end
