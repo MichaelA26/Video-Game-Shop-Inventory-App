5061aae5b0b35a63bccefb79cef32bda62841a1d
Michael Alexander <michael_alexander_20@outlook.com>
2019-08-26 10:19:30 +0100

    console.rb and seeds.rb updated

---
+++ video_game_shop/db/seeds.rb
@@ -0,0 +1,30 @@
+require_relative( "../models/console.rb" )
+require_relative( "../models/game.rb" )
+require("pry")
+
+# Game.delete_all()
+Console.delete_all()
+
+console1 = Console.new({
+  "name" => "Xbox One",
+  "manufacturer" => "Microsoft"
+})
+
+console1.save()
+
+console2 = Console.new({
+  "name" => "PS4",
+  "manufacturer" => "Sony"
+})
+
+console2.save()
+
+console3 = Console.new({
+  "name" => "Switch",
+  "manufacturer" => "Nintendo"
+})
+
+console3.save()
+
+binding.pry
+nil
--- video_game_shop/models/console.rb
+++ video_game_shop/models/console.rb
@@ -10,4 +10,33 @@ class Console
     @manufacturer = options['manufacturer']
   end
 
+  def save
+    sql = "INSERT INTO console
+    (name, manufacturer)
+    VALUES($1, $2)
+    RETURNING id"
+    values =[@name, @manufacturer]
+    results = SqlRunner.run(sql, values)
+    @id = results.first()['id'].to_i
+  end
+
+  def self.all()
+    sql = "SELECT * FROM consoles"
+    results = SqlRunner.run(sql)
+    return results.map { |hash| Console.new(hash) }
+  end
+
+  def self.find( id )
+    sql = "SELECT * FROM consoles
+    WHERE id = $1"
+    values = [id]
+    results = SqlRunner.run( sql, values )
+    return Console.new( results.first )
+  end
+
+  def self.delete_all
+    sql = "DELETE FROM consoles"
+    SqlRunner.run( sql )
+  end
+
 end
