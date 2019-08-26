c74cf10535771f7b48adae101f76222708142114
Michael Alexander <michael_alexander_20@outlook.com>
2019-08-26 11:28:44 +0100

    Write console_controller

---
+++ video_game_shop/controllers/console_controller.rb
@@ -0,0 +1,14 @@
+require('sinatra')
+require('sinatra/contrib/all')
+require_relative('../models/console.rb')
+also_reload('../models/*')
+
+get '/consoles' do
+  @consoles = Console.all()
+  erb ( :"consoles/index" )
+end
+
+get '/consoles/:id' do
+  @consoles = Console.find(params['id'].to_i)
+  erb(:"consoles/show")
+end
--- video_game_shop/controllers/game_controller.rb
+++ video_game_shop/controllers/game_controller.rb
@@ -4,11 +4,11 @@ require_relative('../models/game.rb')
 also_reload('../models/*')
 
 get '/games' do
-  @zombies = Game.all()
+  @games = Game.all()
   erb ( :"games/index" )
 end
 
 get '/games/:id' do
-  @game = Game.find(params['id'].to_i)
+  @games = Game.find(params['id'].to_i)
   erb(:"games/show")
 end
