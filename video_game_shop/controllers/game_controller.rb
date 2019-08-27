3777cc41657f0bbc8c0aa7f1d2e6fb7e8674fc11
Michael Alexander <michael_alexander_20@outlook.com>
2019-08-27 14:10:48 +0100

    Add create, delete etc to game_controller

--- video_game_shop/controllers/game_controller.rb
+++ video_game_shop/controllers/game_controller.rb
@@ -12,3 +12,29 @@ get '/games/:id' do
   @games = Game.find(params['id'].to_i)
   erb(:"game/show")
 end
+
+get '/games/new' do
+  erb(:new)
+end
+
+post '/games' do
+  @game = Game.new(params)
+  @game.save()
+  erb(:create)
+end
+
+get '/games/:id/edit' do
+  @game = Game.find(params[:id])
+  erb(:edit)
+end
+
+post '/games/:id' do
+  Game.new(params).update
+  redirect to '/games'
+end
+
+post '/games/:id/delete' do
+  game = Game.find(params[:id])
+  game.delete()
+  redirect to '/game'
+end
